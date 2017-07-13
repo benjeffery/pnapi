84fcb638623c28cbe838a4bbca30f6bcaab55a9f
Ben Jeffery <ben.jeffery@well.ox.ac.uk>
2017-07-13 13:14:13 +0100

    array buffer decoding

--- test_pnAPI.py
+++ test_pnAPI.py
@@ -6,16 +6,16 @@ class TestPnAPI(TestCase):
     def setUp(self):
         self.api = PnAPI('Pf60_Internal', 'USER', 'PASS')
 
-    # def test__getconfig(self):
-    #     self.api._getconfig()
-    #
-    # def test_avaliableProperties(self):
-    #     print(self.api.avaliableProperties())
-    #
-    # def test_avaliable2DProperties(self):
-    #     print(self.api.avaliable2DProperties())
+    def test__getconfig(self):
+        self.api._getconfig()
+
+    def test_avaliableProperties(self):
+        print(self.api.avaliableProperties())
+
+    def test_avaliable2DProperties(self):
+        print(self.api.avaliable2DProperties())
 
     def test_get2D(self):
-        print(self.api.get2D('genotypes', ['calldata/genotype', 'calldata/DP'], ['POS'], ['Sample'],
+        print(self.api.get2D('genotypes', ['calldata/genotype', 'calldata/AD'], ['POS'], ['Sample'],
                              '{"whcClass":"compound","isCompound":true,"isRoot":true,"Components":[{"whcClass":"comparefixed","isCompound":false,"ColName":"POS","CompValue":1000,"Tpe":"<"},{"whcClass":"comparefixed","isCompound":false,"ColName":"CHROM","CompValue":"Pf3D7_01_v3","ColName2":"AutoKey","Tpe":"="}],"Tpe":"AND"}',
                              None, 'POS', 'Sample'))
